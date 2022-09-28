b9bc581e5ce1cdac8a4ab95a8bb4d3a47c9c5772
Robert Shepley <shepleysound@gmail.com>
2022-09-27 19:16:29 -0700

    Change error handling functions.

--- src/error-handlers/404.js
+++ src/error-handlers/404.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = (req,res,next) => {
-  let error = { error: 'Resource Not Found' };
-  res.statusCode = 404;
-  res.statusMessage = 'Not Found';
-  res.setHeader('Content-Type', 'application/json');
-  res.write(JSON.stringify(error));
-  res.end();
+  res.status(404)
+    .render('Resource Not Found');
 };
--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,10 +1,6 @@
 'use strict';
 
 module.exports = (err, req, res, next) => {
-  let error = { error: err.message || err };
-  res.statusCode = err.status || 500;
-  res.statusMessage = err.statusMessage || 'Server Error';
-  res.setHeader('Content-Type', 'application/json');
-  res.write(JSON.stringify(error));
-  res.end();
+  res.status(500)
+    .render('Server Error', { error: err.message || err });
 };
